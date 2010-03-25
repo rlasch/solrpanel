4018ce724ff97cc147d3f23e24970452e4880b27
Robert Lasch <rlasch@gmail.com>
2010-03-24 22:27:52 -0500

    Added dynamic aatributes to result.

--- lib/sunlight.rb
+++ lib/sunlight.rb
@@ -14,26 +14,31 @@ module Sunlight
     
     attr_accessor :base_url
     
+    
     def initialize(url = 'http://localhost:8983/solr/')
       @base_url = url
       @base_url = @base_url + '/' if (@base_url =~ /[^\/]+$/) 
     end
     
+    
     def search(term, params = {})
       client = HTTPClient.new
-      parameters = { :q => term }.merge(params)
+      parameters = { :q => term, :f => "*,company_id" }.merge(params)
       response = client.post(url('select'), parameters)
 
       parse(response)
     end
     
+    
     def update(path, id, params = {})
       response = nil
       File.open(path) do |file|
-        body = { 'myfile' => file, 'literal.id' => id }
-        body.merge(params)
+        body = { 'myfile' => file}
+        query_string = {'literal.id' => id }
+        query_string.merge!(params)
+        puts "query string: #{query_string.inspect}"
         client = HTTPClient.new
-        response = client.post(url('update/extract'), body)
+        response = client.request('POST', url('update/extract'), query_string, body)
       end
    
     end
@@ -46,6 +51,7 @@ module Sunlight
       params = {'Content-Type' => 'text/xml'}
       response = client.request('POST', url('update'), nil, body, params)
     end
+    
     
     def commit()
       response = nil
@@ -68,10 +74,24 @@ module Sunlight
       xml = Nokogiri::XML(response.body.content)
       xml.xpath('//doc').each() do |doc|
         r = Sunlight::Result.new()
-        r.author = doc.xpath("//str[@name='author']").inner_text
-        r.content_type = doc.xpath("arr[@name='content_type']/str").inner_text
-        r.doc_id = doc.xpath("str[@name='id']").inner_text
-        r.title = doc.xpath("arr[@name='title']").inner_text
+        
+        doc.traverse() do |element|
+          name = element['name'] if element['name']
+          if (name != 'arr')
+            value = element.content
+          end
+          
+          if value && name
+            r.send(name + "=", value)
+            name = nil
+            value = nil
+          end
+        end
+        
+        # r.author = doc.xpath("//str[@name='author']").inner_text
+        # r.content_type = doc.xpath("arr[@name='content_type']/str").inner_text
+        # r.doc_id = doc.xpath("str[@name='id']").inner_text
+        # r.title = doc.xpath("arr[@name='title']").inner_text
         results << r
       end
 
--- lib/sunlight/result.rb
+++ lib/sunlight/result.rb
@@ -6,7 +6,20 @@ module Sunlight
     attr_accessor :author, :content_type, :doc_id, :title
     
     def initialize
-      
+      @attrs = {}
+    end
+    
+    def method_missing(id, value)
+      str = id.id2name
+      if str =~ /=$/
+        @attrs[id] = value
+      else
+        if @attrs[id]
+          return @attrs[id] 
+        else
+          return nil
+        end
+      end
     end
   end
   
